da7040331efb1e297cf2e95eaea861e206defd87
Cwaihenya <cwaihenya.gmail.com>
2021-06-19 17:07:40 +0900

    More formatting, review of different diplays

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,15 @@
 const { environment } = require('@rails/webpacker')
 const coffee =  require('./loaders/coffee')
 
+const webpack = require('webpack')
+
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  // uncomment below for bootstrap 4.x
+  // Popper: ['popper.js', 'default']
+  // uncomment below for bootstrap 5
+  Popper: ['@popperjs/core', 'default']
+}))
 environment.loaders.prepend('coffee', coffee)
 module.exports = environment
